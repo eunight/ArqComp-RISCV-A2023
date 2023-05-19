d1a8cb8b6d5588dd3d39bb622593fc480d59acf7
eunight <125163689+eunight@users.noreply.github.com>
2023-05-18 23:16:35 -0400

    MUX.h

--- Execute/3-MUX/MUX.h
+++ Execute/3-MUX/MUX.h
@@ -6,7 +6,7 @@
 class mux: public sc_module{
 public:
 	sc_in<bool> aIn, bIn;
-	sc_in<sc_int<32>> Op1_In, Op2_In, Op3_In;
+	sc_in<sc_int<32>> Op1_In, Op2_In;
 	sc_out<sc_int<32>> Op_Out;
 
 	SC_CTOR(Mux);
